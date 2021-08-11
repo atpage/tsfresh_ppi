95b36560f2526c1856399ea283a0741e85da670d
Alex Page <a.t.page@gmail.com>
2021-08-11 11:21:41 -0400

    Add description to setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 import setuptools
 
-# with open("README.md", "r", encoding="utf-8") as fh:
-#     long_description = fh.read()
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setuptools.setup(
     name="tsfresh_ppi",
     version="2021.8.10",
     author="Alex Page",
     author_email="a.t.page@gmail.com",
-    # description="A small example package",
-    # long_description=long_description,
-    # long_description_content_type="text/markdown",
+    description="Add peak-to-peak interval features to tsfresh",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/atpage/tsfresh_ppi",
     project_urls={
         "Bug Tracker": "https://github.com/atpage/tsfresh_ppi/issues",
